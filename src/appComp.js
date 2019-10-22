fd7eb48c08de51e848e9d229e2e09edbae247265
hmsajal <19496424+hmsajal@users.noreply.github.com>
2019-10-23 02:46:33 +0600

    huge change

---
+++ src/appComp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './appComp.css';
+import Header from './header'
+import Section from './section'
+
+
+export default function appComp(props){    
+     
+        return(
+            <div>
+                <Header/>
+                <Section/>
+            </div>
+        )    
+}
---
+++ src/section.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Section(props){
+    return(
+        <section>
+
+            <h2>A very few things to add</h2>
+
+            <p>Suddenly I've noticed that there are a very few things to add to this bio type website. This is a kind of website which I have been wishing to have for many days. But for an anxiety patient who procrastinates all the time, it is not unexpected to delay. But you see at last I've made it thanks to github. The beautiful but simple theme template by <a href="https://github.com/orderedlist">orderedlist</a> should also be mentined.</p>
+            
+            <h2>Chronicles of a chronic disorder </h2>
+
+
+            <h2>Childhood, Frustration and Antifrustration</h2>
+            <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
+            <p id="bye">Good Bye</p>
+
+       </section>
+    )
+    
+}
