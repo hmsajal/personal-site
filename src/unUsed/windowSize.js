18516f2da6c1d99dcf73aa5f6b63c3ef1256c477
hmsajal <19496424+hmsajal@users.noreply.github.com>
2019-11-18 22:05:29 +0600

    babel and babeli

--- src/unUsed/windowSize.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-wh = window.innerHeight;
-ww = window.innerWidth;
-
-export {wh,ww}
---
+++ test.js
@@ -0,0 +1,7 @@
+test = () =>{
+    const arr = [2,3,4,5]
+    const arr2= arr.map(a=>a)
+ 
+    return arr2;
+} 
+console.log(test())
