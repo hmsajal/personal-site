9558da801152684cef22cdbc0fb664ec7f3ef028
hmsajal <19496424+hmsajal@users.noreply.github.com>
2020-02-26 03:28:05 +0600

    added nav menu to bio page

--- src/components/bio/bioAnchor.js
+++ src/components/bio/bioAnchor.js
@@ -1,32 +1,22 @@
 import React from 'react'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faEnvelope} from '@fortawesome/free-regular-svg-icons'
-import {faFacebookF,faGithub,faLinkedinIn,faAngellist} from '@fortawesome/free-brands-svg-icons'
+
 
 import styles from './bioAnchor.module.css'
 
 function BioAnchor() {
     return(        
         <div className={styles.outerDiv}>              
-               <a>
-                  <FontAwesomeIcon icon={faLinkedinIn} />
+               <a className={styles.anchor} href="#">
+                  intro
                </a>                                                       
 
-               <a>
-                  <FontAwesomeIcon icon={faEnvelope} />
+               <a className={styles.anchor} href="#">
+                  photos                  
                </a>                                                                  
                
-               <a>
-                  <FontAwesomeIcon icon={faGithub}/>
-               </a>                                        
-                          
-               <a>
-                   <FontAwesomeIcon icon={faFacebookF}/>  
-               </a>                                        
-                          
-               <a>
-                   <FontAwesomeIcon icon={faAngellist} />  
-               </a>                                                           
+               <a className={styles.anchor} href="#">
+                  contact
+               </a>                                                                                                                                   
         </div>  
     )
 }
