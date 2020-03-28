7960f1ed5de1adff183fe8b0655d74e81e7d14cb
hmsajal <19496424+hmsajal@users.noreply.github.com>
2020-03-28 21:09:43 +0600

    improvement in biobasic

--- src/components/bio/bioBasicInfo.js
+++ src/components/bio/bioBasicInfo.js
@@ -1,4 +1,5 @@
 import React,{Fragment} from 'react'
+import {NavLink} from 'react-router-dom'
 
 import styles from './bioBasicInfo.module.css'
 import ProfileLinks from './bioProfileLinks'
@@ -8,21 +9,46 @@ import MyImage from '../common/myImage'
 export default function BioBasicInfo(props){             
     return (
         <>                        
-            <div className={styles.main} id="bioBasicInfo">                                       
-                <div className={styles.imgParaDiv}>                                                        
+            <div className={styles.main} id="bioBasicInfo">                                                               
+                <div className={styles.imgParaDiv}>
                     <div className={styles.myImageDiv}>
                        <MyImage imgWidth="120px"/>            
                     </div>        
-                    <div className={styles.paraDiv}>
-                        <p>
-                            I am Hasan Mahmud Sajal. This is my portfolio site. This portion of the site is kind of an About Me option of a profile of any social networking site. Just for who are interested. Are you?!
-                            <br/><br/>I am an app developer who is expert in frontend web and mobile application development and I love to do what I do.                
-                        </p>
-                    </div>                                                                  
-                </div>                                                                                                              
+                    <div className={styles.paraDiv}>                        
+                        <Init/>                   
+                    </div>                    
+                </div>                  
+
+                <Decl/>                                  
+                <h4 className={styles.uiDivider}>Interests</h4>
+                
+                <div className={styles.interests}>                    
+                    <Interests heading="Favourite Topics" icon="book" item1="Philosophy" item2="Science"
+                               item3="Human Nature"
+                    >
+                        <div className="item list">Technology</div>
+                    </Interests>                    
+                </div>                          
+                <div className={styles.interests}>                    
+                    <Interests heading="Movies or Series" icon="film" item1="Fight Club"
+                               item3="Interstellar" item2="Pulp Fiction"
+                    >
+                        <div className="item list">Breaking Bad</div>
+                        <div className="item list">Game of Thrones</div>
+                    </Interests>                    
+                </div>    
+                <div className={styles.interests}>                    
+                    <Interests heading="People I envy" icon="users" item1="Rabindranath Tagore"
+                               item2="Steve Jobs" item3="Christopher Nolan"
+                    >
+                        <div className="item list">Mahmudul Hasan Sohag</div>                        
+                    </Interests>                    
+                </div>                                                                                                                                                                                                     
             </div>  
+
+
             <div className={styles.profileLinksDiv}>
-            <ProfileLinks/>
+               <ProfileLinks/>
             </div>
         </>               
     )        
@@ -30,12 +56,44 @@ export default function BioBasicInfo(props){
 
 
 
-const desc = () => {
-
+const Init = () => {
     return(
-        <>                              
-               <br/><br/>I am an app developer who is expert in frontend web and mobile application development and I love to do what I do. 
-               <br/>I am interested in Science, Human Nature and Philosophy.
-        </>
+        <p>                              
+          I am Hasan Mahmud Sajal and this is my portfolio site. As you're already surfing my site the probability 
+          is high that you know me. By the way, in this section I am supposed to introduce myself. But the irony
+          is, it's not possible to know someone from a couple of words which are told by the person himself/herself. 
+          <br/>But I can tell you my personality type. I think it is much information in a very short form and 
+          my personality type is INFP.          
+        </p>
     )
 }
+
+const Decl = () => {
+    return(
+        <div style={{padding:'40px 0px 30px 0px',fontSize:'15px',textAlign:'justify',textJustify:'auto'}}>
+            This page is sort of the ABOUT ME section of any social networking site profile. 
+            So it's intended for only those who are interested about me as a person. Are you? 
+            Then go through the following facts and other information about me.
+            <p style={{paddingTop:"20px",fontSize:"15px",color:'#666'}}>If you want to know about my career interest, please visit the <NavLink to="/career">Career</NavLink> page</p>
+        </div>
+    )
+}
+
+const Interests = (props) => {
+    return(
+        <div className="ui item" style={{marginBottom:"30px"}}>
+            <h3 className="ui header">
+                <i className={`ui ${props.icon} icon`}></i>
+                <div className="content">
+                    {props.heading}
+                </div>                
+            </h3> 
+            <div className="ui content bulleted list">                    
+                <div className="item list">{props.item1}</div> 
+                <div className="item list">{props.item2}</div> 
+                <div className="item list">{props.item3}</div>
+                {props.children}                             
+            </div>                   
+        </div>
+    )
+}            
