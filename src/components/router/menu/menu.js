1906174e30651c40327bb10b60a0c0b5944fce39
hmsajal <19496424+hmsajal@users.noreply.github.com>
2020-04-08 00:10:52 +0600

    made menu responsibly render component.

--- src/components/router/menu/menu.js
+++ src/components/router/menu/menu.js
@@ -2,34 +2,30 @@ import React from 'react'
 import {Link,NavLink} from 'react-router-dom'
 
 import styles from './menu.module.css'
-import navStyles from './navMenu.module.css'
+import navStyles from './nav.module.css'
 
 
 export default function Menu(props) {    
 
     return (           
-            <div className={styles.mainDiv} id="menu">
-                <div className={styles.myNameDiv}>
-                    <Link className={styles.myName} to="/">
-                       HASAN MAHMUD
-                    </Link>
-                </div>
-                <div className={styles.balancer}></div>
-                <div className={styles.navDiv}>
-                    <ChooseNavMenu/>
-                </div>                
+            <div className={styles.mainDiv} id="menu">                
+                <Link className={styles.myName} to="/">
+                   HASAN MAHMUD
+                </Link>                                
+                <NavMenu/>
+                <NavBar/>
             </div>                  
     )
 }
 
-function ChooseNavMenu(props){                
-    const widthMatching = window.matchMedia("(min-width:1025px)")
-    return widthMatching.matches ? <NavMenu/> : <NavMenu/>    
-}
+// function ChooseNav(props){                
+//     const widthMatching = window.matchMedia("(min-width:1025px)")
+//     return widthMatching.matches ? <NavMenu/> : <NavBar/>    
+// }
 
 
 function NavMenu(){
-    return(        
+    return(             
         <nav className={navStyles.navList}>            
             <NavLink className={navStyles.link} to="/bio" >
                 Bio
@@ -42,6 +38,15 @@ function NavMenu(){
             <NavLink className={navStyles.link} to="/blog" >
                 Blog
             </NavLink>
-        </nav>           
+        </nav>                    
+    )
+}
+
+
+function NavBar(){
+    return(
+        <button className="ui button icon navbutton">
+            <i className="bars icon"></i>
+        </button>
     )
 }
